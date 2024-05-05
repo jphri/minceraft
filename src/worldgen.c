1a292ced7f81ac41bbd176eee2a760fbbf84e2bd
João Pedro Hrihorowitsch Silva <jp.hri.silva@gmail.com>
2024-05-04 22:40:12 -0300

    heightmap interpolation via splines

--- src/worldgen.c
+++ src/worldgen.c
@@ -8,7 +8,17 @@
 #define X_SCALE (0.0625 / CHUNK_SIZE)
 #define Z_SCALE (0.0625 / CHUNK_SIZE)
 
-float octaved2(float x, float z, int seed);
+#define GROUND_HEIGHT 64
+
+typedef struct {
+	float x, y;
+} SplinePoint;
+
+static float octaved2(float x, float z, int seed);
+static float spline(float in, size_t nsplines, SplinePoint *splines);
+static float map(float l, float xmin, float xmax, float ymin, float ymax);
+
+static float heightmap(float x, float z, int seed);
 
 static PCG32State basic_seed;
 static uint32_t   heightmap_seed;
@@ -31,7 +41,7 @@ wgen_generate(int cx, int cy, int cz)
 		int xx = x + cx;
 		int zz = z + cz;
 		
-		float height = octaved2(xx, zz, heightmap_seed) * 8 + 64;
+		float height = heightmap(xx, zz, heightmap_seed);
 		for(int y = 0; y < CHUNK_SIZE; y++) {
 			int yy = y + cy;
 			if(yy <= height) {
@@ -55,4 +65,37 @@ octaved2(float x, float z, int seed)
 	}
 
 	return r / 4;
+}
+
+float
+map(float l, float xmin, float xmax, float ymin, float ymax)
+{
+	const float slope = (ymax - ymin) / (xmax - xmin);
+	return ymin + (l - xmin) * slope;
+}
+
+float
+spline(float in, size_t nsplines, SplinePoint *splines)
+{
+	for(size_t i = 1; i < nsplines; i++) {
+		if(splines[i].x > in && splines[i - 1].x < in) {
+			return map(in, splines[i - 1].x, splines[i].x, splines[i - 1].y, splines[i].y);
+		}
+	}
+	return 0;
+}
+
+float
+heightmap(float x, float z, int seed)
+{
+	static SplinePoint splines[] = {
+		{ -1.00, GROUND_HEIGHT - 20 },
+		{ -0.50, GROUND_HEIGHT - 10 },
+		{ -0.40, GROUND_HEIGHT - 2   },
+		{  0.40, GROUND_HEIGHT + 2   },
+		{  0.60, GROUND_HEIGHT + 10  },
+		{  0.95, GROUND_HEIGHT + 15 }
+	};
+
+	return spline(octaved2(x, z, seed), LENGTH(splines), splines);
 }
