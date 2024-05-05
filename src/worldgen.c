4b67018c1ac918cc42b0e3bc7761007dc6223d81
João Pedro Hrihorowitsch Silva <jp.hri.silva@gmail.com>
2024-05-04 22:49:52 -0300

    3d noise

--- src/worldgen.c
+++ src/worldgen.c
@@ -3,9 +3,11 @@
 #include "world.h"
 
 #include <stdio.h>
+#include <linmath.h>
 #include <noise1234.h>
 
 #define X_SCALE (0.0625 / CHUNK_SIZE)
+#define Y_SCALE (0.0625 / CHUNK_SIZE)
 #define Z_SCALE (0.0625 / CHUNK_SIZE)
 
 #define GROUND_HEIGHT 64
@@ -15,6 +17,7 @@ typedef struct {
 } SplinePoint;
 
 static float octaved2(float x, float z, int seed);
+static float octaved3(vec3 v, int seed);
 static float spline(float in, size_t nsplines, SplinePoint *splines);
 static float map(float l, float xmin, float xmax, float ymin, float ymax);
 
@@ -22,6 +25,7 @@ static float heightmap(float x, float z, int seed);
 
 static PCG32State basic_seed;
 static uint32_t   heightmap_seed;
+static uint32_t   density_seed;
 
 void 
 wgen_set_seed(const char *seed)
@@ -30,6 +34,7 @@ wgen_set_seed(const char *seed)
 	init_pcg32(&basic_seed);
 
 	heightmap_seed = rand_pcg32(&basic_seed);
+	density_seed   = rand_pcg32(&basic_seed);
 }
 
 void
@@ -44,7 +49,10 @@ wgen_generate(int cx, int cy, int cz)
 		float height = heightmap(xx, zz, heightmap_seed);
 		for(int y = 0; y < CHUNK_SIZE; y++) {
 			int yy = y + cy;
-			if(yy <= height) {
+
+			float density = octaved3((vec3){ xx, yy, zz }, density_seed) + (height - yy) * 2.25 / GROUND_HEIGHT;
+
+			if(density > 0) {
 				world_set_block(xx, yy, zz, BLOCK_GRASS);
 			} else {
 				world_set_block(xx, yy, zz, BLOCK_NULL);
@@ -61,6 +69,20 @@ octaved2(float x, float z, int seed)
 
 	for(int i = 0; i < 8; i++) {
 		r += noise3(x * a * X_SCALE, z * a * X_SCALE, seed) * a;
+		a *= 0.5;
+	}
+
+	return r / 4;
+}
+
+float
+octaved3(vec3 position, int seed)
+{
+	float a = 4.0;
+	float r = 0.0;
+
+	for(int i = 0; i < 8; i++) {
+		r += noise4(position[0] * a * X_SCALE, position[1] * a * Y_SCALE, position[2] * a * Z_SCALE, seed) * a;
 		a *= 0.5;
 	}
 
